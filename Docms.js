36815cd6aaa2cee8de764664fcdfd1c7474adc2b
lglong519 <769141564@qq.com>
2017-11-21 22:01:25 +0800

    Update Docms.js

--- Docms.js
+++ Docms.js
@@ -619,18 +619,38 @@
 			}
 		}
 	}
-	Docms.cookie=function(name,val){
-		val=val||'';
-		if(val){
-			document.cookie=name+'='+encodeURIComponent(val);
-		}else{
-			var reg=new RegExp('('+name+')=(.*?)($|(?=;))'),
-				arr=document.cookie.match(reg);
-			//返回值:["name=val", "name", "val"]
-			if(arr){
-				return decodeURIComponent(arr[2]);
+	//读取/设置 cookie,参数:1.名称,2.值,3.过期时间:格式为[yMdhms]+数字,例如:d2表示两天,h3表示3小时,大M是月,小m是分
+	Docms.cookie=function(name,val,exp){
+		name=name||"";
+		exp=exp||"";
+		exp=exp.match(/([yMdhms]{1})(\d+)/);
+		//如果设置了过期时间,将exp转换成对应时间的毫秒
+		if(exp){
+			switch(exp[1]){
+				case "y":exp=new Date().setFullYear(new Date().getFullYear()+exp[2]*1);break;
+				case "M":exp=new Date().setMonth(new Date().getMonth()+exp[2]*1);break;
+				case "d":exp=new Date().setDate(new Date().getDate()+exp[2]*1);break;
+				case "h":exp=new Date().setHours(new Date().getHours()+exp[2]*1);break;
+				case "m":exp=new Date().setMinutes(new Date().getMinutes()+exp[2]*1);break;
+				case "s":exp=new Date().setSeconds(new Date().getSeconds()+exp[2]*1);
 			}
-			return null;
+		}
+		if(name){
+			if(typeof(val)=='string'){
+				exp?document.cookie=name+'='+encodeURIComponent(val)+';expires='+new Date(exp).toUTCString():
+				document.cookie=name+'='+encodeURIComponent(val);
+				return !0;
+			}else{
+				var reg=new RegExp('('+name+')=(.*?)($|(?=;))'),
+					arr=document.cookie.match(reg);
+				//返回值:["name=val", "name", "val"]
+				if(arr){
+					return decodeURIComponent(arr[2]);
+				}
+				return null;
+			}
+		}else{
+			return document.cookie;
 		}
 	}
 	function jsonToGet(data) {
